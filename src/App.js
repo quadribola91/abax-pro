bc3713ee6aadd3b7929616f487519f880eec3152
Developer <quadribola91@gmail.com>
2026-02-13 16:35:53 +0100

    3.1

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import HeroSection from "./components/HeroSection";
+import VisionMissionSection from "./components/VisionMissionSection";
+import ServicesSection from "./components/Services";
+import AboutSection from "./components/AboutSection";
 import About from "./pages/About";
 import ContactSection from "./components/ContactSection";
-import TeamPage from "./pages/TeamPage";
+import TeamSection from "./components/TeamSection";
+import WhyUs from "./components/WhyUs";
 import FooterSection from "./components/FooterSection";
 import Home from "./pages/Home";
+import TeamPage from "./pages/TeamPage";
 import ScrollToTop from "./components/ScrollToTop";
 import ServicesPage from "./pages/ServicesPage";
 
 function App() {
   return (
     <Router>
+      {/* Navbar appears on all pages */}
+      <Navbar />
       <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<ContactSection />} />
+        <Route path="/team" element={<TeamPage />} />{" "}
+        <Route path="/servicepage" element={<ServicesPage />} />
+      </Routes>
 
-      {/* App Background */}
-      <div className="bg-gray-50 min-h-screen">
-        {/* Fixed Width Wrapper */}
-        <div className="max-w-7xl mx-auto px-4 md:px-6">
-          {/* Navbar */}
-          <Navbar />
-
-          {/* Routes */}
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/contact" element={<ContactSection />} />
-            <Route path="/team" element={<TeamPage />} />
-            <Route path="/servicepage" element={<ServicesPage />} />
-          </Routes>
-
-          {/* Footer */}
-          <FooterSection />
-        </div>
-      </div>
+      {/* Footer appears on all pages */}
+      <FooterSection />
     </Router>
   );
 }
