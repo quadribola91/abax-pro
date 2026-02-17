0fc2a87e764f4e846d939d832b888ad9db95b862
Developer <quadribola91@gmail.com>
2026-02-17 17:00:33 +0100

    4.5.1

--- src/pages/CallToAction.js
+++ src/pages/CallToAction.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function CallToAction() {
   return (
-    <section className="py-20 bg-gray-50 text-black text-center">
+    <section className="py-20 bg-gray-200 text-black text-center">
       <div className="max-w-4xl mx-auto px-6">
         <h2 className="text-xl md:text-4xl font-bold mb-6">
           Ready to Partner with Abax Professional Services?
@@ -15,7 +15,7 @@ export default function CallToAction() {
 
         <a
           href="/contact"
-          className="bg-white text-blue-900 px-8 py-3 rounded-lg font-semibold hover:bg-blue-300 transition duration-300"
+          className="bg-gray-200 text-blue-900 px-8 py-3 rounded-lg font-semibold hover:bg-blue-300 transition duration-300"
         >
           Schedule a Consultation
         </a>
